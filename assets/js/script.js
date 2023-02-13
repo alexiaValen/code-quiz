83fbcca0fc3e21fee2958f2f5cb8dd3072fda106
Alexia Valenzuela <alexiavalenzuela@Alexias-MacBook-Air.local>
2023-02-13 13:10:33 -0700

    setting up the html layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,55 @@
-//CODE QUIZ PROMPTS:
-// 1. What does HTML stand for?
-// A. Hyper Trainer Marking Language
-// B. Hyper Text Marketing Language
-// C. Hyper Text Markup Language
-// D. Hyper Text Markup Leveler
-// answer: "c"
+//VAR WITH QUIZ QUESTIONS AND ANSWERS:
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
+        answer: "c"
+    },
+    {
+        title: "Which is the correct way to attach a stylesheet to HTML?",
+        choices:[""],
+        answer: ""
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["Alerts", "Booleans", "Strings", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["Commas", "Quotes", "Curly Brackets", "Parenthesis"],
+        answer: "Quotes"
+    },
+    {
+        title: "What is the name of the object that allows you to perform mathematical tasks with the interpreter?",
+        choices: ["Solve", "Count", "Number", "Math"],
+        answer: "Math"
+    },
+    {
+        title: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+        choices: ["Clone", "Loop", "Repeat", "Constant"],
+        answer: "Loop"
+    },
+    {
+        title: "What kind of statement is used to execute actions based on a trigger or condition?",
+        choices: ["RegExp or Regular Expression", "Fired Event", "Conditional Statement", "Boolean Variable"],
+        answer: "Conditional Statement"
+    },
+    {
+        title: "What is a JavaScript element that represents either TRUE or FALSE values?",
+        choices: ["Boolean", "Condition", "Event", "RegExp"],
+        answer: "Boolean"
+    },
+    {
+        title: "CSS stands for ____ Style Sheets.",
+        choices: ["Concave", "Concept", "Create", "Cascading"],
+        answer: "Cascading"
+    },
+    {
+        title: "The link element must go inside the ____ section of an HTML document or page.",
+        choices: ["Body", "Head", "Paragraph", "Footer"],
+        answer: "Head"
+    },
+];
 
-// 2. Which is the correct way to attach a stylesheet to HTML?
-// A. <link href="style.css" src="stylehseet"/>
-// B. <link rel="stylesheet" src="style.css" />
-// C. <link rel="style.css" href="stylesheet"/>
-// D. <link rel="styleheet" href="style.css"/>
-// answer: "c"
-
-// 3. Commonly used data types DO NOT include:
-// A. Alerts
-// B. Booleans
-// C. Strings
-// D. Numbers
-// answer: "a"
-
-// 4. String values must be enclosed within ____ when being assigned to variables.
-// A. Commas
-// B. Quotes
-// C. Curly Brackets
-// D. Parenthesis
-// answer: "b"
-
-// 5. What is the name of the object that allows you to perform mathematical tasks with the interpreter?
-// A. Solve
-// B. Count
-// C. Number
-// D. Math
-// answer: "d"
-
-// 6. What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?
-// A. Clone
-// B. Loop
-// C. Repeat
-// D. Constant
-// answer: "b"
-
-// 7. What kind of statement is used to execute actions based on a trigger or condition?
-// A. RegExp or Regular Expression
-// B. Fired Event
-// C. Conditional Statement
-// D. Boolean Variable
-// answer: "c"
-
-// 8. What is a JavaScript element that represents either TRUE or FALSE values?
-// A. Boolean
-// B. Condition
-// C. Event
-// D. RegExp
-// answer: "a"
-
-// 9. CSS stands for ____ Style Sheets.
-// A. Concave
-// B. Concept
-// C. Create
-// D. Cascading
-// answer: "d"
-
-// 10. The link element must go inside the ____ section of an HTML document or page.
-// A. Body
-// B. Head
-// C. Paragraph
-// D. Footer
-// answer: "b"
 
