a0b863a8c555bd712618e25eb5747ee387aa4cb2
Alexia Valenzuela <alexiavalenzuela@Alexias-MacBook-Air.local>
2023-02-13 10:15:16 -0700

    created quiz questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+//CODE QUIZ PROMPTS:
+// 1. What does HTML stand for?
+// A. Hyper Trainer Marking Language
+// B. Hyper Text Marketing Language
+// C. Hyper Text Markup Language
+// D. Hyper Text Markup Leveler
+// answer: "c"
+
+// 2. Which is the correct way to attach a stylesheet to HTML?
+// A. <link href="style.css" src="stylehseet"/>
+// B. <link rel="stylesheet" src="style.css" />
+// C. <link rel="style.css" href="stylesheet"/>
+// D. <link rel="styleheet" href="style.css"/>
+// answer: "c"
+
+// 3. Commonly used data types DO NOT include:
+// A. Alerts
+// B. Booleans
+// C. Strings
+// D. Numbers
+// answer: "a"
+
+// 4. String values must be enclosed within ____ when being assigned to variables.
+// A. Commas
+// B. Quotes
+// C. Curly Brackets
+// D. Parenthesis
+// answer: "b"
+
+// 5. What is the name of the object that allows you to perform mathematical tasks with the interpreter?
+// A. Solve
+// B. Count
+// C. Number
+// D. Math
+// answer: "d"
+
+// 6. What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?
+// A. Clone
+// B. Loop
+// C. Repeat
+// D. Constant
+// answer: "b"
+
+// 7. What kind of statement is used to execute actions based on a trigger or condition?
+// A. RegExp or Regular Expression
+// B. Fired Event
+// C. Conditional Statement
+// D. Boolean Variable
+// answer: "c"
+
+// 8. What is a JavaScript element that represents either TRUE or FALSE values?
+// A. Boolean
+// B. Condition
+// C. Event
+// D. RegExp
+// answer: "a"
+
+// 9. CSS stands for ____ Style Sheets.
+// A. Concave
+// B. Concept
+// C. Create
+// D. Cascading
+// answer: "d"
+
+// 10. The link element must go inside the ____ section of an HTML document or page.
+// A. Body
+// B. Head
+// C. Paragraph
+// D. Footer
+// answer: "b"
+
